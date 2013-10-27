885d620c13795012609d7f7ff70a82f9e7ce6775
Tyrone Paige <tlpaige@gmail.com>
2013-10-26 23:18:11 -0400

    I'm not exactly sure what is going to happen here since I was trying to commit a file and it seems I need to commit the entire repo

--- internetGitHubProject/src/internetGitHubProject/NiceAndEasy.java
+++ internetGitHubProject/src/internetGitHubProject/NiceAndEasy.java
@@ -6,6 +6,9 @@ public class NiceAndEasy {
 		// TODO Auto-generated method stub
 
 		System.out.println ("Ok message");
+		System.out.println ("And now a better message");
+		System.out.println ("Not sure what happened to the second print out but here the their print line.");
+		
 		
 		
 	}
