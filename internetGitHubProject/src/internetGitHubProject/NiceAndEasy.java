5ae44883224963240e90c268eb11ff7854aa7287
Tyrone Paige <tlpaige@gmail.com>
2013-10-26 22:40:09 -0400

    My first project which I will send to Internet Github

---
+++ internetGitHubProject/src/internetGitHubProject/NiceAndEasy.java
@@ -0,0 +1,13 @@
+package internetGitHubProject;
+
+public class NiceAndEasy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println ("Ok message");
+		
+		
+	}
+
+}
